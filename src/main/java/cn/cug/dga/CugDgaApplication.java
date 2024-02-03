5f45ca6cfecd748bbe6635bcbb756d152923abca
songsong <songsjps@163.com>
2024-02-03 16:14:51 +0800

    数据治理项目第一次提交到本地库

---
+++ src/main/java/cn/cug/dga/CugDgaApplication.java
@@ -0,0 +1,13 @@
+package cn.cug.dga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CugDgaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CugDgaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/cug/dga/CugDgaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cug.dga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CugDgaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
